7168028124a14e83ccdc9b6fdbdf5d0be3b35620
PraveenRaghu <praveen.raghu.raghu@gmail.com>
2025-12-14 22:50:07 +0530

    -m "Introduced config manager for dev,QA,uat environment
    Update pom.xml: update source and target to Java 14

--- src/test/java/com/api/tests/LoginAPITest.java
+++ src/test/java/com/api/tests/LoginAPITest.java
@@ -1,26 +1,29 @@
 package com.api.tests;
 
 import static io.restassured.RestAssured.given;
+import static io.restassured.http.ContentType.JSON;
+import static org.hamcrest.Matchers.equalTo;
+import static org.hamcrest.Matchers.lessThan;
 
-import static org.hamcrest.Matchers.*;
+import java.io.IOException;
 
 import org.testng.annotations.Test;
 
 import com.api.pojo.UserCredentials;
+import static com.api.utils.ConfigManager.*;
 
 import io.restassured.module.jsv.JsonSchemaValidator;
-
-import static io.restassured.http.ContentType.*;
 
 public class LoginAPITest {
 	
 	@Test
-	public void loginAPITest() {
+	public void loginAPITest() throws IOException {
+		
 		
 		UserCredentials userCredentials = new UserCredentials("iamfd", "password");
 		
 		given()
-			.baseUri("http://64.227.160.186:9000/v1")
+			.baseUri(getProperty("BASE_URI"))
 		.and()
 			.contentType(JSON)
 			.accept(JSON)
