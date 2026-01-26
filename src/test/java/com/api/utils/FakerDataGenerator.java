2f5e0a77942a6904ee24fc9dfac763240fb2407f
PraveenRaghu <praveen.raghu.raghu@gmail.com>
2026-01-26 17:28:55 +0530

    Fixed random for Problem id

--- src/test/java/com/api/utils/FakerDataGenerator.java
+++ src/test/java/com/api/utils/FakerDataGenerator.java
@@ -25,6 +25,7 @@ public class FakerDataGenerator {
 	private static final int MST_OEM_ID= 1;
 	private static final int PRODUCT_ID =1; 
 	private static final int MST_MODEL_ID=1;
+	private static final int validProblemID[] = {1,2,3,4,5,6,7,8,9,10,11,12,15,16,17,19,20,22,24,26,27,28,29};
 	 
 	private FakerDataGenerator(){
 		
@@ -59,9 +60,9 @@ public class FakerDataGenerator {
 	private static List<Problems> generateFakeProblemList() {
 		String fakeRemark = faker.lorem().sentence(5);
 		
-		int problemID = RANDOM.nextInt(27)+1;
+		int randomIndex = RANDOM.nextInt(validProblemID.length);
 		
-		Problems problems = new Problems(problemID, fakeRemark);
+		Problems problems = new Problems(validProblemID[randomIndex], fakeRemark);
 		
 		List<Problems> problemList = new ArrayList<Problems>();
 		problemList.add(problems);
