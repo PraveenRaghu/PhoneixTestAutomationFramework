2c859e61182c16ed3c73af229fdd3ed9808fb827
PraveenRaghu <praveen.raghu.raghu@gmail.com>
2026-01-27 23:22:56 +0530

    Introduced JSON reader utility to read JSON TestData for loginAPI

--- src/test/java/com/dataproviders/DataProviderUtils.java
+++ src/test/java/com/dataproviders/DataProviderUtils.java
@@ -7,9 +7,11 @@ import java.util.List;
 import org.testng.annotations.DataProvider;
 
 import com.api.request.model.CreateJobPayload;
+import com.api.request.model.UserCredentials;
 import com.api.utils.CSVReaderUtil;
 import com.api.utils.CreateJobBeanMapper;
 import com.api.utils.FakerDataGenerator;
+import com.api.utils.JSONReaderUtil;
 import com.dataproviders.api.bean.CreateJobBean;
 import com.dataproviders.api.bean.UserBean;
 
@@ -44,4 +46,11 @@ public class DataProviderUtils {
     	Iterator<CreateJobPayload>payloadIterator= FakerDataGenerator.generateFakeCreateJobData(fakerCountInt);
     	return payloadIterator;
      }
+     
+     @DataProvider(name= "LoginAPIJSONDataprovider",parallel=true)
+ 	public static Iterator<UserCredentials> loginAPIJSONDataProvider() {
+     	 
+     	return JSONReaderUtil.loadJSON("testData/LoginAPITestData.json",UserCredentials[].class);
+ 		
+ 	}
 }
