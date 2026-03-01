74487857c2fcfa61349a7bd419ef9803d548a91e
PraveenRaghu <praveen.raghu.raghu@gmail.com>
2026-03-01 23:26:09 +0530

    API Test listeners added to Login test class

--- src/test/java/com/api/tests/LoginAPITest.java
+++ src/test/java/com/api/tests/LoginAPITest.java
@@ -7,11 +7,12 @@ import static org.hamcrest.Matchers.equalTo;
 import java.io.IOException;
 
 import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Listeners;
 import org.testng.annotations.Test;
 
 import com.api.request.model.UserCredentials;
 import com.api.services.AuthServices;
-
+@Listeners(com.listeners.APITestListener.class)
 public class LoginAPITest {
 	private UserCredentials userCredentials;
 	 private AuthServices authserivce;
